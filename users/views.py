72f3cead6f1292f958feb8014ee9567fd94a106e
Arthur Tanaka <arthurtanaka.dev@gmail.com>
2026-05-06 23:07:24 -0300

    feat: add usuario, serializer junto com views e urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Usuario
+from .serializers import UsuarioSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+    
+    def get_permissions(self):
+        """
+        Permite qualquer um criar conta (registro).
+        Demais actions exigem autenticação.
+        """
+        if self.action == 'create':
+            return [AllowAny()]
+        return [IsAuthenticated()]
