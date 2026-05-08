02475b67a01f53abba3b8b86aeb82b74961e09bf
Arthur Tanaka <arthurtanaka.dev@gmail.com>
2026-05-08 17:07:56 -0300

    feat: add filtro de agendamentos por usuario e permissao IsOwnerOnly

--- agendamentos/views.py
+++ agendamentos/views.py
@@ -13,4 +13,8 @@ class AgendamentoViewSet(viewsets.ModelViewSet):
         # Define o cliente como o usuário autenticado
         serializer.save(cliente=self.request.user)
         
+    def get_queryset(self):
+        # Retorna apenas os agendamentos do cliente autenticado
+        return Agendamento.objects.filter(cliente=self.request.user)
+        
     permission_classes = [IsAuthenticated, IsOwnerOnly]
