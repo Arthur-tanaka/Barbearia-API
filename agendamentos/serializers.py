74cc072ab47b2a72289a69cdaa9cd0987e8ad98c
Arthur Tanaka <arthurtanaka.dev@gmail.com>
2026-05-09 00:46:32 -0300

    feat: add validações de agendamento e horario

--- agendamentos/serializers.py
+++ agendamentos/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 from .models import Agendamento
+from django.utils import timezone
 
 class AgendamentoSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         return Agendamento.objects.create(**validated_data)
+    def validate_data(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("A data do agendamento deve ser no futuro.")
+        return value
+    def validate(self, data):
+        # Verifica se o barbeiro já tem um agendamento no mesmo horário
+        if Agendamento.objects.filter(barbeiro=data['barbeiro'], data=data['data']).exists():
+            raise serializers.ValidationError("O barbeiro já tem um agendamento nesse horário.")
+        return data
     
     class Meta:
         model = Agendamento
