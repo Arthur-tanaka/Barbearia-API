05669e566d4b1944427da713b5d6d196fe23ecb7
Arthur Tanaka <arthurtanaka.dev@gmail.com>
2026-05-08 22:16:53 -0300

    feat: add testes automatizados para agendamentos

--- agendamentos/tests.py
+++ agendamentos/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from users.models import Usuario
+from barbeiros.models import Barbeiro
 
-# Create your tests here.
+
+class AgendamentosTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.usuario = Usuario.objects.create_user(username='testuser', password='testpassword', tipo_usuario='cliente')
+        self.barbeiro = Usuario.objects.create_user(username='testbarber', password='testpassword', tipo_usuario='barbeiro')
+        self.barbeiro_obj = Barbeiro.objects.create(
+            usuario = self.barbeiro,
+            nome = 'Barbeiro Teste',
+            servico_oferecido = 'Corte de Cabelo',
+        )
+        # Autentica o cliente
+        response = self.client.post('/api/token/', {
+            'username': 'testuser',
+            'password': 'testpassword',
+        })
+        self.token = response.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+        
+    def test_create_agendamento(self):
+        response = self.client.post('/api/agendamentos/', {
+            'barbeiro': self.barbeiro_obj.id,
+            'data': '2024-07-01T10:00:00Z'
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['cliente'], self.usuario.id)
